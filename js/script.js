0990097e7febe303ca97f94bd36dfcac1cba727a
abdulawalemon1 <emondx4@gmail.com>
2022-02-16 00:58:07 +0600

    code optimized

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
-document.body.style.background = '#1E293B';
-document.body.style.color = '#94A38B';
+// document.body.style.background = '#1E293B';
+// document.body.style.color = '#94A38B';
 
+
+onClick('eightGB', 'memory-cost', 0);
 onClick('sixteenGB', 'memory-cost', 300);
+onClick('ssd1', 'storage-cost', 0);
 onClick('ssd2', 'storage-cost', 400);
 onClick('ssd3', 'storage-cost', 500);
+onClick('free-delivery', 'delivery-cost', 0);
 onClick('paid-delivery', 'delivery-cost', 20);
 
 
@@ -11,6 +15,7 @@ onClick('paid-delivery', 'delivery-cost', 20);
 function onClick(clickId, updateId, price) {
     document.getElementById(clickId).addEventListener('click', function () {
         updatePrice(updateId, price);
+        document.getElementById(clickId).style.backgroundColor = "cornflowerblue";
     })
 }
 
@@ -32,3 +37,19 @@ function updatePrice(itemId, price) {
     const total = document.getElementById('total-price');
     total.innerText = totalPrice;
 }
+
+const fakecode = 'pHero';
+
+document.getElementById('apply-btn').addEventListener('click', function () {
+    const code = document.getElementById('promo-input').value;
+    if (code === fakecode) {
+        const total = document.getElementById('total-price');
+        let totalPrice = parseFloat(total.innerText);
+        const discount = totalPrice * 0.2;
+        totalPrice = totalPrice - discount;
+        total.innerText = totalPrice;
+
+    } else {
+
+    }
+})
